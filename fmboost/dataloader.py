477f818b019474bc369909a87293f43b8df7f52a
benearnthof <benearnthof@hotmail.de>
2025-04-02 17:59:52 +0200

    add generic image dataset to dataloader.py

--- fmboost/dataloader.py
+++ fmboost/dataloader.py
@@ -10,6 +10,59 @@ from torch.utils.data import DataLoader
 
 from fmboost.helpers import instantiate_from_config
 from fmboost.helpers import load_partial_from_config
+from fmboost.helpers import exists
+
+from pathlib import Path
+from typing import List
+
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+from torch import nn
+
+from PIL import Image
+
+class ImageDataset(Dataset):
+    def __init__(
+        self,
+        folder: str | Path,
+        image_size: int,
+        exts: List[str] = ['jpg', 'jpeg', 'png', 'tiff'],
+        augment_horizontal_flip = False,
+        convert_image_to = None
+    ):
+        super().__init__()
+        if isinstance(folder, str):
+            folder = Path(folder)
+
+        assert folder.is_dir()
+
+        self.folder = folder
+        self.image_size = image_size
+
+        self.paths = [p for ext in exts for p in folder.glob(f'**/*.{ext}')]
+
+        self.maybe_convert_fn = partial(self.convert_image_to_fn, convert_image_to) if exists(convert_image_to) else nn.Identity()
+
+        self.transform = T.Compose([
+            T.Lambda(self.maybe_convert_fn),
+            T.Resize(image_size),
+            T.RandomHorizontalFlip() if augment_horizontal_flip else nn.Identity(),
+            T.CenterCrop(image_size),
+            T.ToTensor()
+        ])
+    
+    def convert_image_to_fn(self, img_type, image):
+        if image.mode == img_type:
+            return image
+        return image.convert(img_type)
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(path)
+        return self.transform(img)
 
 
 class DataModuleFromConfig(pl.LightningDataModule):
